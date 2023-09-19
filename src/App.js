f2afba09b9d8724c5f539d6bcae5ec3b7e8034a4
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-09-19 13:15:34 -0400

    error fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+         <h1>Hello World!</h1>
         <h1>Welcome to my card</h1>
         <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
         <div className="paragraph">
@@ -14,11 +14,11 @@ function App() {
         </div>
         <div class="btn-wrapper">
  <a href="https://hax.psu.edu">
-   <button>Details</button> 
+   <button>Details</button> </a> 
    <button id="btn">Duplicator</button>
   <button id="colorBtn">BGColorChanger</button>
   <button id="Headingbtn">HeadingChanger</button>
-  <button id="dltbtn">DeleteCard</button></a> </div>
+  <button id="dltbtn">DeleteCard</button></div>
       </div>
     );
   }
