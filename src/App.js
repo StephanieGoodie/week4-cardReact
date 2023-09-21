5a25748aea849e80fd541081f8f109de13998473
Stephanie Goodie-Obi <goodieobistephanie@gmail.com>
2023-09-20 20:15:47 -0400

    froggy hate

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <h1>Welcome to my card</h1>
         <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
         <div className="paragraph">
-          <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
+          <p>I really hate his frog</p>
         </div>
         <div class="btn-wrapper">
  <a href="https://hax.psu.edu">
